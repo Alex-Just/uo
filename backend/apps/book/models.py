5be7923d73be4b11483be57d35fd711b52fabd90
Alex Just <AlexJustEmail@gmail.com>
2017-08-22 16:14:24 +0300

    Fix bug with bad import

--- backend/apps/book/models.py
+++ backend/apps/book/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-from django_extensions.db.models import TimeStampedModel
 
 from apps.common.behaviors import Publishable
 from apps.common.fields import CustomForeignKey
