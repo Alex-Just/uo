5aa9a7336dbb4b324a3e10cbd9a67edb2b7a700b
Alex Just <AlexJustEmail@gmail.com>
2017-09-29 23:55:13 +0300

    Update react-boilerplate

--- frontend/app/containers/App/index.js
+++ frontend/app/containers/App/index.js
@@ -1,30 +1,24 @@
-import PropTypes from 'prop-types';
 /**
  *
- * App.react.js
+ * App.js
  *
  * This component is the skeleton around the actual pages, and should only
  * contain code that should be seen on all pages. (e.g. navigation bar)
- *
- * NOTE: while this component should technically be a stateless functional
- * component (SFC), hot reloading does not currently support SFCs. If hot
- * reloading is not a necessity for you then you can refactor it and remove
- * the linting exception.
  */
 
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
-export default class App extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+import HomePage from 'containers/HomePage/Loadable';
+import NotFoundPage from 'containers/NotFoundPage/Loadable';
 
-  static propTypes = {
-    children: PropTypes.node,
-  };
-
-  render() {
-    return (
-      <div>
-        {React.Children.toArray(this.props.children)}
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </div>
+  );
 }
